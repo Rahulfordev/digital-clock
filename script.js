9dc858b7fd3c75b33b83d5d6acc1fc1fba477817
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2023-08-26 00:17:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function clock() {
+  let hour = document.getElementById("hours");
+  let minute = document.getElementById("minutes");
+  let second = document.getElementById("seconds");
+  let ampm = document.getElementById("ampm");
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let am = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    am = "PM";
+  }
+  if (h < 10) {
+    h = "0" + h;
+  }
+  if (m < 10) {
+    m = "0" + m;
+  }
+  if (s < 10) {
+    s = "0" + s;
+  }
+
+  hour.innerHTML = h;
+  minute.innerHTML = m;
+  second.innerHTML = s;
+  ampm.innerHTML = am;
+}
+
+clock();
+
+let interval = setInterval(clock, 1000);
